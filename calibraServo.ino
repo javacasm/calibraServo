7af7e0047d5939e383bf372cd903d364a5f937d8
javacasm <javacasm@gmail.com>
2015-03-09 19:11:17 +0100

    potenciometer vs joystick

--- calibraServo.ino
+++ calibraServo.ino
@@ -1,33 +1,60 @@
+// V 0.5
 // Calibrado de las posiciones de los servos
 // Basado en http://learn.robotgeek.com/demo-code/120-arduino-linear-actuator-tutorial-incremental-joystick-for-small-linear-actuator.html
 
 #include <Servo.h>
 #include "controlServo.h"
 
-controlServo csCadera(12,A0);
-controlServo csHombro(11,A1);
-controlServo csCodo(10,A2);
-controlServo csMunecaX(9,A3);
-//controlServo csMunecaY(8,A4);
-//controlServo csPinza(7,A5);
+#define nDOF 6
 
+// Vamos a crear una estructura para almacenar una posicion completa
+struct position
+{
+  char name[4];
+  int pos[nDOF];
+};
+
+controlServo csHip(12,A0,"H",1320,false);  // Use potenciometer to control
+controlServo csSoulder(11,A1,"S",1018);
+controlServo csElbow(10,A2,"E",1600);
+controlServo csWristP(9,A3,"WP",1100);
+controlServo csWristR(8,A4,"WR",1023);
+controlServo csGrip(7,A5,"G",1000);
+controlServo servos[]={csHip,csSoulder,csElbow,csWristP,csWristR,csGrip};
 
 void setup() 
 { 
   Serial.begin(9600);
-  csCadera.inicializa();
-  csHombro.inicializa();
-  csCodo.inicializa();
-  csMunecaX.inicializa();
+
+  for(int i=0;i<nDOF;i++)
+  {    servos[i].inicializa();  }
+
+  csSoulder.setSigno(-1);
+  csElbow.setSigno(-1);  
+ /* csHip.inicializa();
+  csSoulder.inicializa();
+  csElbow.inicializa();
+  csWristP.inicializa();
+  csHip.inicializa();
+  csGrip.inicializa();*/
 } 
 
+String strStatusOld="";
 void loop() 
 { 
-  csCadera.checkControlSetServo();
-  csHombro.checkControlSetServo();
-  //csPinza.checkControlSetServo();
-  csCodo.checkControlSetServo();
-  //csMunecaY.checkControlSetServo();
-  csMunecaX.checkControlSetServo();
+/*  csHip.checkControlSetServo();
+  csSoulder.checkControlSetServo();
+  csGrip.checkControlSetServo();
+  csElbow.checkControlSetServo();
+  csWristR.checkControlSetServo();
+  csWristP.checkControlSetServo();*/
   
+  String strStatus="";
+  for(int i=0;i<nDOF;i++)
+  {  strStatus+=servos[i].checkControlSetServo(); }
+  if(strStatus!=strStatusOld)
+  {
+    Serial.println(strStatus);
+    strStatusOld=strStatus;
+  }
 } 
--- controlServo.h
+++ controlServo.h
@@ -1,6 +1,7 @@
 #include <Servo.h>
 #include <Arduino.h>
 
+// V 0.5
 // Calibrado de las posiciones de los servos
 // Basado en http://learn.robotgeek.com/demo-code/120-arduino-linear-actuator-tutorial-incremental-joystick-for-small-linear-actuator.html
 
@@ -8,67 +9,122 @@ class controlServo
 {
 public:
 
-
-	controlServo(int pinServo,int pinControl)
+	controlServo(int pinServo,int pinControl,String id,int iPosInicial,boolean bControlType)
+	{  	    
+          bControledByJoystick=bControlType;  
+          init(pinServo,pinControl,id);
+          iPOS_Inicial=iPosInicial;        
+          iSigno=1;
+        }
+        
+        controlServo(int pinServo,int pinControl,String id,int iPosInicial)
+	{  	    
+          bControledByJoystick=true;
+          init(pinServo,pinControl,id);
+          iPOS_Inicial=iPosInicial;        
+          iSigno=1;
+        }
+        
+        
+	void init(int pinServo,int pinControl,String id)
 	{
-	    iPinControl=pinControl;
-	    iPinServo=pinServo;
-      	iPOS_MIN=75;  //fully retracted
-	    iPOS_MAX=3200;
-
+		iPinControl=pinControl;
+		iPinServo=pinServo;
+		iPOS_MIN=75;  //fully retracted
+		iPOS_MAX=3200;
+		iPOS_Inicial=1100;// (iPOS_MAX+iPOS_MIN)/2;
+		strID=id;
 	}
+        
+	controlServo(int pinServo,int pinControl,String id)
+	{   init(pinServo,pinControl,id); 	}
 
 	int setPosicion(int pos)
 	{
 		servoValue=pos;
 		//usamos writeMicroseconds para hacerlo más preciso		
 		servo.writeMicroseconds(servoValue); 		
-        delay(10); // waits for the servo to get to they're position before continuing 
-        Serial.print(iPinServo);
-        Serial.print(":");
-        Serial.println(servoValue);
-
+		delay(10); // waits for the servo to get to they're position before continuing 
 	}
+
+	void setSigno(int signo)
+	{  iSigno=signo;
+           Serial.print("signo"); 
+           Serial.print(strID);
+           Serial.println(iSigno);
+        }
+
+	String getStatus()
+	{  if(iSigno==-1)	
+            return strID+"(-):"+servoValue+" ";
+           else
+           return strID+":"+servoValue+" ";
+         }
+
+	String getFullStatus()  // Restamos 14 para que se vea en formato A0-A5
+	{	return strID+"@"+iPinServo+", A"+(iPinControl-14)+":"+servoValue+" ";} 
+
 
 	void inicializa()
 	{
 		//initialize servos
 		servo.attach(iPinServo, iPOS_MIN, iPOS_MAX);  // conectamos el servo en este pin y establecemos los valores mínimo y máximo
-		setPosicion((iPOS_MAX+iPOS_MIN)/2);
+		setPosicion(iPOS_Inicial);
 	}
 
-
-
-    void checkControlSetServo() 
-    { 
-      
-        /**************Servo Positions *******************************/
-        // leemos los valores del joystick
-        JSValue = analogRead(iPinControl);
-         
-       
-        // solo actualizamos si estamos por encima de la zona central (para evitar vibraciones)
-        if(JSValue > DEADBANDHIGH || JSValue < DEADBANDLOW)
-        {
-           JSValueMapped = map(JSValue, 0, 1023, -speed, speed); //Mapeamos el valor del joystick (0 to 1023) a (-speed to speed)
-           int newValue= servoValue + JSValueMapped; //add mapped joystick value to present Value
-           if((newValue>iPOS_MIN) && (newValue<iPOS_MAX))
-           {
-             if(newValue!=servoValue) // Solo actualizamos si es distinto. Si hay vibraciones podemos hacer un filtrado
-             {
-                 setPosicion(newValue);
-             }
-           }
-        }
-    }
+	String checkControlSetServo() 
+	{ 
+	  if(bControledByJoystick)
+              return checkControlSetServobyJoystick();
+          else
+              return checkControlSetServobyPotenciometer();
+         }
+    
      
 protected:
+
+        String checkControlSetServobyPotenciometer()
+        {
+          JSValue=analogRead(iPinControl);
+          int newValue= map(JSValue,0,1024,iPOS_MIN,iPOS_MAX);
+          if(abs(newValue-servoValue)>2*speed) // Solo actualizamos si es distinto. Si hay vibraciones podemos hacer un filtrado
+	  {
+		setPosicion(newValue);
+       	  }
+	  return getStatus();
+        }
+        
+        String checkControlSetServobyJoystick() 
+	{ 
+		/**************Servo Positions *******************************/
+		// leemos los valores del joystick
+		JSValue = analogRead(iPinControl);
+
+		// solo actualizamos si estamos por encima de la zona central (para evitar vibraciones)
+		if(JSValue > DEADBANDHIGH || JSValue < DEADBANDLOW)
+		{
+			JSValueMapped = map(JSValue, 0, 1023, -speed, speed); //Mapeamos el valor del joystick (0 to 1023) a (-speed to speed)
+			int newValue= servoValue + iSigno*JSValueMapped; //add mapped joystick value to present Value
+			if((newValue>iPOS_MIN) && (newValue<iPOS_MAX))
+			{
+				if(newValue!=servoValue) // Solo actualizamos si es distinto. Si hay vibraciones podemos hacer un filtrado
+				{
+					setPosicion(newValue);
+				}
+			}
+		}
+		return getStatus(); 
+                //return getFullStatus();       
+        }
 	// Ajuste de la zona central del joytstick para ajustar el 0
 	const static int DEADBANDLOW=482;   //decrease this value if drift occurs, increase it to increase sensitivity around the center position
 	const static int DEADBANDHIGH=542;  //increase this value if drift occurs, decrease it to increase sensitivity around the center position
-
+        
+        boolean bControledByJoystick;
+        
 	int iPOS_MIN;  //fully retracted
 	int iPOS_MAX; //fully extended
+	int iPOS_Inicial; // Initial Position (empezamos con el valor medio)
 
 	Servo servo;    // servo
 
@@ -76,13 +132,15 @@ protected:
 
 	int JSValueMapped;        // Se convierte el valor leido en el rango del servo
 
-	int servoValue;   // posicion actual (empezamos con el valor medio)
+	int servoValue;   // posicion actual 
 
-	const static int speed = 5;        // velocidad a la que cambiamos la posición. Para hacerlo fino usar 1
+	const static int speed = 25;        // velocidad a la que cambiamos la posición. Para hacerlo fino usar 1
 
+	int iSigno;
 	int iPinServo; // Pin al que está conectado el servo
 
 	int iPinControl; // Pin analógico al que se conecta el control
 
+    String strID;
 
 };
